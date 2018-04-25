c8c81d95610c72fc1b2ce6344a74826e99986b80
colson <colson@cogfactory.net>
2018-04-25 09:15:31 -0700

    add stringify and parse_yaml methods

--- src/ambatch.cr
+++ src/ambatch.cr
@@ -6,34 +6,48 @@ require "yaml"
 # results to the command line. Useful for projects where you want to mock up
 # a structure without commiting to use the generators ahead of time.
 module Ambatch
+
   class Parser
+
+    @commands = [] of String
+
+    def self.parse_yaml(path : String)
+
+      begin
+        raise "Error: File not found." if !File.file? path
+        raise "Error: File extension is not .yml" if File.extname(path).to_s == ".yml"
+        data = YAML.parse(File.read path)
+      rescue msg
+        puts msg
+      end
+    end
+
+    def self.stringify(yaml : YAML::Any | Nil)
+      commands = [] of String
+      if yaml
+        yaml.each do |key, value|
+          value.each do |k, v|
+            result = ""
+            result += key.to_s.downcase + " " + k.to_s + " " + v.to_a.join(" ")
+            commands.push(result)
+          end
+        end
+        commands
+      end
+    end
+
     def self.run
       OptionParser.parse! do |parser|
         parser.banner = "Usage: ambatch -f /path/to/file.yml"
         parser.on "-f PATH", "--file PATH", "Path to a file" do |path|
           # TODO clean this parsing up
           begin
-            data = YAML.parse(File.read path)
-            lines = [] of String
-            if data
-              data.each do |key, value|
-                value.each do |k, v|
-                  result = ""
-                  result += key.to_s.downcase + " " + k.to_s + " " + v.to_a.join(" ")
-                  lines.push(result)
-                end
-              end
-
-              lines.each do |line|
-                puts system "amber g " + line
-              end
-            end
-
-            # TODO it would be helpful to know the types of erros this creates...
-          rescue YAML::ParseException
-            raise "Could not parse file"
+            commands = self.stringify(self.parse_yaml(path))
+            commands.each do |line|
+              puts system "amber g " + line
+            end if commands
           rescue ex
-            raise "Some other error occurred"
+            raise "#{ex}"
           end
         end
         parser.on "-h", "--help", "Show this help" do
